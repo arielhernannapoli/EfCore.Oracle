39d17b1cf71b8977c56a826338934d4f0dee8814
ahnapoli@gmail.com <ahnapoli@gmail.com>
2020-04-17 22:30:02 -0300

    Init

---
+++ EfCore.Oracle.Example/Infrastructure/DbContext/BloggingContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCore.Oracle.Example.Infrastructure
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseOracle(@"User Id=ARIEL;Password=Tano3320;Data Source=localhost:1521/example");
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
